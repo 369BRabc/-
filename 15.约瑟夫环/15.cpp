f3bbe8ac9285dca429356f27265029098823311e
zhaoyang <congzhaoyang1@163.com>
2019-03-15 09:51:48 +0800

    update

--- 15.约瑟夫环/15.cpp
+++ 15.约瑟夫环/15.cpp
@@ -3,38 +3,39 @@
 */
 
 #include <iostream>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-// n总人数，m报数间隔，s开始下标、进行中位置
 void solve(int n, int m, int s) {
-  int say_num = 1, cnt = 0; // 正在报数，已删除人数
-  int a[n]; // 奇数数组状态，如果a[i] = -1代表删除
-  for(int i = 0; i < n; i++) a[i] = i;
-  while(cnt < n) { // 已删除的人比n小
-    while(say_num < m) {
-      s = (s + 1) % n; // 向后推
-      if(a[s] != -1) say_num++;
-    }
-    cout << s << endl;
-    cnt++; // 第s个人报数为m
-    a[s] = -1; // 修改被删除的位置值为-1
-    if(cnt < n) {
-      say_num = 1; // 初始化say_num
-      while(a[s] == -1) { // 将s移动到有数字的位置
-        s = (s + 1) % n;
-      }
-    }
-  }
+	vector<int> v(n);
+	for(int i = 0; i < n; i++) v[i] = i; // 标记数组，删除后改为-1
+	int outnum = 0; // 出局人数
+	int saynum = 1; // 正在报出的数字
+	while(outnum < n){
+		while(saynum < m) {
+			s = (s + 1) % n; // 向后推进
+			if(v[s] != -1) saynum++;
+		}
+		// 找到了报数为m的位置
+		cout << "delete:" << s + 1 << endl;
+		v[s] = -1;
+		outnum++;
+		if(outnum < n) {
+			saynum = 1;
+			while(v[s] == -1) s = (s + 1) % n;
+		}
+	}
 }
 
 int main() {
-  int m, n, s;
-  cout << "input m n s:" << endl;
-  scanf("%d %d %d", &m, &n, &s);
-  solve(m, n, s);
-  system("pause");
-  return 0;
+	int n, m, s; // n总数，m间隔，s开始位置
+	cout << "intpu n, m, s:" << endl;
+	scanf("%d %d %d", &n, &m, &s);
+	solve(n, m, s);
+	system("pause");
+	return 0;
 }
